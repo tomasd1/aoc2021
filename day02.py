58be42ef8a59348f812313360399182590a184d9
Tomas Drahokoupil <tomas.drahokoupil@gmail.com>
2021-12-02 19:56:06 +0100

    day 2

---
+++ day02.py
@@ -0,0 +1,26 @@
+# Advent of Code 2021, day 2
+# PART 1
+acc = {"depth": 0, "pos": 0}
+
+def down(n): acc["depth"] += n
+def up(n): acc["depth"] -= n
+def forward(n): acc["pos"] += n
+
+for line in [line.rstrip() for line in open('data02.txt')]:
+    command, n = line.split()
+    locals()[command](int(n))
+
+print(acc["depth"]*acc["pos"])
+
+# PART 1
+acc = {"depth": 0, "pos": 0, "aim": 0}
+
+def down(n): acc["aim"] += n
+def up(n): acc["aim"] -= n
+def forward(n): acc["pos"] += n; acc["depth"] += acc["aim"] * n;
+
+for line in [line.rstrip() for line in open('data02.txt')]:
+    command, n = line.split()
+    locals()[command](int(n))
+
+print(acc["depth"]*acc["pos"])
